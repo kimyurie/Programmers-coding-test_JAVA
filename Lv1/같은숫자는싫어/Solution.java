86f3cf35b42e20dd7d9895229d7cf20df0572820
unknown <9911dbfl@naver.com>
2024-09-14 13:10:49 +0900

    [Add]: stack/queue_같은 숫자는 싫어

---
+++ Lv1/같은숫자는싫어/Solution.java
@@ -0,0 +1,21 @@
+package Lv1.같은숫자는싫어;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        // 스택 생성
+        Stack<Integer> stack = new Stack<>();
+
+        // 스택에 arr 속 값 넣기
+        for(int i : arr) {
+            // 스택이 비어있거나, 스택 최상단 값이 현재 i와 같지 않으면
+            if(stack.empty() || !stack.peek().equals(i)) {
+                // 스택에 집어넣기
+                stack.push(i);
+            }
+        }
+
+        return stack.stream().mapToInt(i -> i).toArray();
+    }
+}
