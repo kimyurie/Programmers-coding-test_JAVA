9ec14a3078ae2a6a4e5d80c5f9e87c44cb462964
unknown <9911dbfl@naver.com>
2024-09-15 11:02:41 +0900

    [Add]: queue_다리를 지나는 트럭

---
+++ Lv2/다리를지나는트럭/Solution.java
@@ -0,0 +1,62 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42583
+// https://minhamina.tistory.com/241
+package Lv2.다리를지나는트럭;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        // 현재 다리 위에 있는 트럭들의 무게 합
+        int sum = 0;
+        // 경과 시간
+        int time = 0;
+
+        // 모든 트럭을 순차적으로 다리에 올리는 작업을 진행
+        for(int i = 0; i < truck_weights.length; i++) {
+            // 현재 트럭의 무게를 truck에 저장
+            int truck = truck_weights[i];
+
+            // 현재 트럭을 다리에 올리기 전에 가능한지 반복적으로 확인
+            while(true) {
+                // 1. 다리 위에 트럭이 없는 경우
+                if(queue.isEmpty()){
+                    queue.add(truck);
+                    sum += truck;
+
+                    // 트럭이 다리에 오를 때 시간을 1초 추가
+                    time++;
+                    break; // 반복 종료 후 다음 트럭으로
+                }
+                // 2. 다리위에 트럭있지만 다리가 꽉 찬 경우
+                else if(queue.size() == bridge_length) {
+                    // 다리 위 가장 앞에 있는 트럭 다리 다 건너면 큐에서 제거
+                    sum -= queue.poll();
+                }
+                // 3. 다리위에 트럭 있고, 다리 꽉 차지 않은 경우
+                else{
+                    // 3-1. 새로운 트럭 다리에 올려도 무게 한도 넘지 않는 경우
+                    if(sum + truck <= weight) {
+                        // 트럭을 다리에 올리고, 다리위 트럭 무게합에 더함
+                        queue.add(truck);
+                        sum += truck;
+                        // 트럭 다리에 오를 때 시간 1초 추가
+                        time++;
+                        break; // 반복 종료하고 다음 트럭으로
+                    }
+                    // 3-2. 새로운 트럭 다리에 올리면 무게 한도 넘는 경우
+                    else{
+                        // 다리가 다 찰 때까지 0을 넣음
+                        // 다리 위 트럭 움직이도록 시간만 증가
+                        queue.add(0);
+                        time++;
+                    }
+                }
+            }
+        }
+        // 모든 트럭 다리 건넌 후에도 마지막 트럭 다리 건너야함
+        // 마지막 트럭이 다리에서 빠져나가는 시간 더하기
+        return time + bridge_length;
+    }
+}
