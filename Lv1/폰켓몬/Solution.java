6f3d6ff9f4009b1d261d51f9352edba286b34415
unknown <9911dbfl@naver.com>
2024-09-19 19:21:40 +0900

    [Refactor]: review_hash_폰켓몬

--- Lv1/폰켓몬/Solution.java
+++ Lv1/폰켓몬/Solution.java
@@ -1,24 +1,49 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1845
+// reveiw : 1(sol)
 package Lv1.폰켓몬;
 
+// m1)
+//import java.util.*;
+//
+//class Solution {
+//    public int solution(int[] nums) {
+//        HashSet<Integer> set = new HashSet<>();
+//
+//        for(int i = 0; i < nums.length; i++) {
+//            set.add(nums[i]);
+//
+//        }
+//        // 3 1 2   ---> 4 / 2 = 2
+//        if(nums.length / 2 < set.size()){
+//            return nums.length / 2;
+//        }
+//        // 3 2 4 ---> 6 / 2 = 3
+//        // 3 2
+//        else {
+//            return set.size();
+//        }
+//    }
+//}
+
+// review m2)
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
+
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             set.add(nums[i]);
 
+            if (nums.length / 2 >= set.size()) {
+                answer = set.size();
+            }
         }
-        // 3 1 2   ---> 4 / 2 = 2
-        if(nums.length / 2 < set.size()){
-            return nums.length / 2;
-        }
-        // 3 2 4 ---> 6 / 2 = 3
-        // 3 2
-        else {
-            return set.size();
-        }
+
+        return answer;
     }
 }
+
+
