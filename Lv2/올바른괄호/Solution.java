e38d24e11a0a9fcd288217f83e74844e273401b9
unknown <9911dbfl@naver.com>
2024-09-29 19:29:55 +0900

    [Refactor]: rv_stack_올바른 괄호

--- Lv2/올바른괄호/Solution.java
+++ Lv2/올바른괄호/Solution.java
@@ -1,6 +1,8 @@
-package Lv2.올바른괄호;
-
 // https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java
+package Lv2.올바른괄호;
+// review : 1(o)
+// char를 String으로 변환
+// : Character.toString(char) 또는 String.valueOf(char)을 사용
 
 import java.util.*;
 
@@ -22,3 +24,34 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+
+
+//class Solution {
+//    boolean solution(String s) {
+//        boolean answer = true;
+//
+//        Stack<String> stack = new Stack<>();
+//
+//        for(int i = 0; i < s.length(); i++) {
+//            if(stack.empty()) {
+//                if(s.charAt(i) == ')') {
+//                    return false;
+//                }else{
+//                    stack.push(String.valueOf(s.charAt(i)));
+//                }
+//            }else { // "("
+//                if(s.charAt(i) == ')') {
+//                    stack.pop();
+//                }else{
+//                    stack.push(String.valueOf(s.charAt(i)));
+//                }
+//            }
+//        }
+//
+//        if(stack.empty()) answer = true;
+//        else answer = false;
+//
+//        return answer;
+//    }
+//}
