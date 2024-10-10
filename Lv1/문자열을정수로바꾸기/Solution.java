65738ebce1bda28eb0c18c260848c7e8d97a950e
unknown <9911dbfl@naver.com>
2024-10-10 17:55:28 +0900

    [Add]: string_문자열을 정수로 바꾸기

---
+++ Lv1/문자열을정수로바꾸기/Solution.java
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925?language=java
+package Lv1.문자열을정수로바꾸기;
+
+class Solution {
+    public int solution(String s) {
+        // 문자열 -> 정수 : Integer.parseInt();
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
