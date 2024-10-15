41c0a4fc04b0a4e68feb562a5746e1c6c32b2815
unknown <9911dbfl@naver.com>
2024-10-15 15:34:43 +0900

    [Refactor]: rv(1)_dfs_모음사전

--- Lv2/모음사전/Solution.java
+++ Lv2/모음사전/Solution.java
@@ -1,5 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/84512
 package Lv2.모음사전;
+// review : 1
 
 import java.util.*;
 
@@ -7,26 +8,25 @@ class Solution {
     static List<String> list; // 모든 단어 저장할 리스트
     static String[] words = {"A", "E", "I", "O", "U"};
 
-    // 재귀적으로 모든 단어를 생성하는 함수
-    static void dfs(String str, int len) {
+    // 1. 재귀적으로 모든 단어를 생성하는 함수
+    static void dfs(String str, int depth) {
         list.add(str);
-        if(len == 5) return;
+        if(depth == 5) return;
 
         // 5개 모음 각각 붙여 재귀적으로 단어 생성
         for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1); // str에 모음 붙여 다음 단계로 재귀 호출
+            dfs(str + words[i], depth + 1); // str에 모음 붙여 다음 단계로 재귀 호출
         }
     }
 
-    // 주어진 단어가 사전에서 몇번째 위치인지 찾음
+    // 2. 주어진 단어가 사전에서 몇번째 위치인지 찾음
     public int solution(String word) {
         int answer = 0;
         list = new ArrayList<>();
         dfs("", 0);
 
         // list에서 주어진 단어가 몇번째 위치에 있는지 찾음
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(word)) {
                 answer = i;
                 break;
