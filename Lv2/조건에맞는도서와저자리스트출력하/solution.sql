64abc2acc50194e8d3323a7e6b61656fcc8a75f6
unknown <9911dbfl@naver.com>
2024-10-17 12:36:49 +0900

    [Add]: SQL_조건에 맞는 도서와 저자 리스트 출력하기

---
+++ Lv2/조건에맞는도서와저자리스트출력하/solution.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144854#
+SELECT
+    A.BOOK_ID,
+    B.AUTHOR_NAME,
+    DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+FROM
+    BOOK A JOIN AUTHOR B ON A.AUTHOR_ID = B.AUTHOR_ID
+WHERE A.CATEGORY = '경제'
+ORDER BY A.PUBLISHED_DATE ASC;
